1b3edefbf908b28a7c92febb4bab84f6d0323803
Premdatascience <premkumar.datascience@gmail.com>
2022-10-18 09:47:03 +0530

    search update

--- src/FormVal/ViewFormval.js
+++ src/FormVal/ViewFormval.js
@@ -7,24 +7,40 @@ import { XLSX } from "xlsx";
 import { CSVLink } from "react-csv";
 import $ from "jquery";
 import Home from "../Home";
+import ReactPaginate from "react-paginate";
 
 const ViewFormval = () => {
   const [data, setData] = useState([]);
-  const [filterdata, setFilterdata] = useState([]);
-  const [query, setQuery] = useState("");
+  //search
+  const [loading, setLoading] = useState(false);
+  const [searchTitle, setSearchTitle] = useState("");
+
   const keys = ["email", "password", "address", "address2", "City", "Zip"];
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // page
+  //
+  // const [page ,setPagecount] =useState([0]);
 
-  const getData = async () => {
-    const response = await axios.get("http://localhost:4000/formview");
-    setData(response.data);
-    console.log(getData);
-    console.log(response.data);
-    setFilterdata(response.data);
+  const getData = async (currentpage) => {
+    setLoading(true);
+    const response = await axios.get(
+      `http://localhost:4000/formview?_page=${currentpage}&_limit=2`
+    );
+    // setData(response.data);
+    // console.log(getData);
+    // console.log(response.data);
+    // setFilterdata(response.data);
+    return response.data;
   };
+
+  useEffect(() => {
+    const resolves = async () => {
+      const contents = await getData();
+      setData(contents);
+      setLoading(false);
+    };
+    resolves();
+  }, []);
 
   const deleteData = async (id) => {
     await axios.delete(`http://localhost:4000/formdelete/${id}`);
@@ -32,21 +48,13 @@ const ViewFormval = () => {
     getData();
   };
 
-  const handlesearch = (event) => {
-    const getSearch = event.target.value;
-    if (getSearch.length > 0) {
-      const searchdata = data.filter(
-        (item) =>
-          // item.email.toLowerCase().includes(getSearch) ||
-          // item.Zip.toLowerCase().includes(getSearch) ||
-          // item.City.toLowerCase().includes(getSearch)
-      keys.some(key=>item[key].toLowerCase().includes(getSearch))
-          );
-      setData(searchdata);
-    } else {
-      setData(filterdata);
-    }
-    setQuery(getSearch);
+  // page
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
+
+    let currentpage = data.selected + 1;
+    const commentsFormserver = await getData(currentpage);
+    setData(commentsFormserver);
   };
 
   return (
@@ -72,14 +80,9 @@ const ViewFormval = () => {
                   </Link>
                 </div>
                 <div className="col">
-                  <input
-                    type="text"
-                    name="name"
-                    value={query}
-                    className="form-control"
-                    onChange={(e) => handlesearch(e)}
-                    placeholder="Search..."
-                  />
+                  <input type="text" name="name"
+                   onChange={(e) => setSearchTitle(e.target.value)}
+                  placeholder="Search..." />
                 </div>
               </div>
             </div>
@@ -101,34 +104,70 @@ const ViewFormval = () => {
                 </tr>
               </thead>
               <tbody>
-                {data.map((formval, index) => (
-                  <tr key={formval.id}>
-                    <td>{index + 1}</td>
-                    <td>{formval.email}</td>
-                    <td>{formval.password}</td>
-                    <td>{formval.address}</td>
-                    <td>{formval.address2}</td>
-                    <td>{formval.City}</td>
-                    <td>{formval.Zip}</td>
+                {loading ? (
+                  <h4>Loading ...</h4>
+                ) : (
+                  data
+                    .filter((value) => {
+                      if (searchTitle === "") {
+                        return value;
+                      } else if (
+                        keys.some((key) =>
+                          value[key]
+                            .toLowerCase()
+                            .includes(searchTitle.toLowerCase())
+                        )
+                      ) {
+                        return value;
+                      }
+                    })
+                    .map((formval, index) => (
+                      <tr key={formval.id}>
+                        <td>{index + 1}</td>
+                        <td>{formval.email}</td>
+                        <td>{formval.password}</td>
+                        <td>{formval.address}</td>
+                        <td>{formval.address2}</td>
+                        <td>{formval.City}</td>
+                        <td>{formval.Zip}</td>
 
-                    <td>
-                      <Link
-                        to={`/formvaledit/${formval._id}`}
-                        className="btn btn-info"
-                      >
-                        Edit
-                      </Link>
-                      <button
-                        onClick={() => deleteData(formval._id)}
-                        className="btn btn-danger"
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
+                        <td>
+                          <Link
+                            to={`/formvaledit/${formval._id}`}
+                            className="btn btn-info"
+                          >
+                            Edit
+                          </Link>
+                          <button
+                            onClick={() => deleteData(formval._id)}
+                            className="btn btn-danger"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                )}
               </tbody>
             </table>
+            <div className="container mt-5">
+              <ReactPaginate
+                previousLabel={"< previous"}
+                nextLabel={"next >"}
+                breakLabel={"..."}
+                pageCount={3}
+                pageRangeDisplayed={3}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                previousClassName={"page-item"}
+                previousLinkClassName={"page-link"}
+                nextClassName={"page-item"}
+                nextLinkClassName={"page-link"}
+                activeClassName={"active"}
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,22 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+{
+  loading ? (
+    <h4>Loading ...</h4>
+  ) : (
+    posts
+      .filter((value) => {
+        if (searchTitle === "") {
+          return value;
+        } else if (
+          value.title.toLowerCase().includes(searchTitle.toLowerCase())
+        ) {
+          return value;
+        }
+      })
+      .map((item) => <h5 key={item.id}>{item.title}</h5>)
+  );
+}
